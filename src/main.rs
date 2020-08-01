5b3078d7f0a7396068232bfec05c44948cb2d7b4
Connor Hawley <connor.hawley4@gmail.com>
2020-07-31 22:47:35 -0400

    Read stdin user input to run program.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,16 @@
+use std::io::stdin;
+
 fn main() {
-  println!("Hello, World!");
+    read_line();
+}
+
+fn read_line() {
+    let mut input = String::new();
+    println!("Primes up to what number should be discovered?");
+    match stdin().read_line(&mut input) {
+        Ok(_n) => {
+            println!("{}", input);
+        }
+        Err(error) => println!("error: {}", error),
+    }
 }
