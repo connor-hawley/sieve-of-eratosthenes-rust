cea7673b0b2ecacaa4c938ae17dfb3a37caf4ddf
Connor Hawley <connor.hawley4@gmail.com>
2020-12-19 20:55:29 -0500

    Add prime number sieve and refactor input read

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,54 @@
-use std::io::stdin;
+use std::io;
+ 
+mod sieve;
+use sieve::Sieve;
+use sieve::Sift;
 
 fn main() {
-    read_line();
+    let evaluation_limit = get_evaluation_limit_from_user();
+    let sieve = Sieve::new(evaluation_limit);
+    println!("Running sieve for numbers up to {}", evaluation_limit);
+    let primes = sieve.sift();
+    println!("Primes up until {}", evaluation_limit);
+    for prime in primes.iter() {
+        println!("{}", prime);
+    }
 }
 
-fn read_line() {
+fn get_evaluation_limit_from_user() -> usize {
+    match read_line() {
+        Ok(evaluation_limit) => evaluation_limit as usize,
+        Err(invalid_input) => {
+            println!("input {} was not a valid integer", invalid_input);
+            get_evaluation_limit_from_user()
+        }
+    }
+}
+
+fn read_line() -> Result<i32, String> {
     let mut input = String::new();
     println!("Primes up to what number should be discovered?");
-    match stdin().read_line(&mut input) {
-        Ok(_n) => {
-            println!("{}", input);
+    match io::stdin().read_line(&mut input) {
+        Ok(_) => {
+            println!("parsing string: \"{}\" as integer", input.trim());
+            parse_integer(input.trim().to_string())
+        },
+        Err(_) => {
+            println!("could not read line");
+            Err(input)
         }
-        Err(error) => println!("error: {}", error),
+    }
+}
+
+fn parse_integer(string_to_convert: String) -> Result<i32, String> {
+    match string_to_convert.parse::<i32>() {
+        Ok(evaluation_limit) => {
+            println!("parsed string {} as integer: {}", string_to_convert, evaluation_limit);
+            Ok(evaluation_limit)
+        },
+        Err(e) => {
+            println!("failed to parse string {} as integer: {}", string_to_convert, e);
+            Err(string_to_convert)
+        }
     }
 }
