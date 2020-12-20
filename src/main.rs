b8afbd94a65433cdfe67f0d46ffc0f7b1d8db101
Connor Hawley <connor.hawley4@gmail.com>
2020-12-19 21:11:55 -0500

    Make output more user-friendly

--- src/main.rs
+++ src/main.rs
@@ -10,14 +10,19 @@ fn main() {
     println!("Running sieve for numbers up to {}", evaluation_limit);
     let primes = sieve.sift();
     println!("Primes up until {}", evaluation_limit);
+    
+    let mut primes_list = String::new();
+    let list_delimeter = ", ";
     for prime in primes.iter() {
-        println!("{}", prime);
+        primes_list.push_str(&prime.to_string());
+        primes_list.push_str(list_delimeter);
     }
+    println!("{}", primes_list);
 }
 
 fn get_evaluation_limit_from_user() -> usize {
     match read_line() {
-        Ok(evaluation_limit) => evaluation_limit as usize,
+        Ok(evaluation_limit) => evaluation_limit,
         Err(invalid_input) => {
             println!("input {} was not a valid integer", invalid_input);
             get_evaluation_limit_from_user()
@@ -25,12 +30,12 @@ fn get_evaluation_limit_from_user() -> usize {
     }
 }
 
-fn read_line() -> Result<i32, String> {
+fn read_line() -> Result<usize, String> {
     let mut input = String::new();
     println!("Primes up to what number should be discovered?");
     match io::stdin().read_line(&mut input) {
         Ok(_) => {
-            println!("parsing string: \"{}\" as integer", input.trim());
+            println!("parsing string: \"{}\" as unsigned integer", input.trim());
             parse_integer(input.trim().to_string())
         },
         Err(_) => {
@@ -40,14 +45,14 @@ fn read_line() -> Result<i32, String> {
     }
 }
 
-fn parse_integer(string_to_convert: String) -> Result<i32, String> {
-    match string_to_convert.parse::<i32>() {
+fn parse_integer(string_to_convert: String) -> Result<usize, String> {
+    match string_to_convert.parse::<usize>() {
         Ok(evaluation_limit) => {
-            println!("parsed string {} as integer: {}", string_to_convert, evaluation_limit);
+            println!("parsed string {} as unsigned integer: {}", string_to_convert, evaluation_limit);
             Ok(evaluation_limit)
         },
         Err(e) => {
-            println!("failed to parse string {} as integer: {}", string_to_convert, e);
+            println!("failed to parse string {} as unsigned integer: {}", string_to_convert, e);
             Err(string_to_convert)
         }
     }
